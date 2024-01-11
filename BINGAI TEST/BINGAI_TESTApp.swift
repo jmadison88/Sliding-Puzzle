3d4b11ef43e587d8e9e9b18410f45f0b45a69a74
Josh Madison <jmad99@sbcglobal.net>
2024-01-11 09:51:50 -0600

    Initial Commit

---
+++ BINGAI TEST/BINGAI_TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BINGAI_TESTApp.swift
+//  BINGAI TEST
+//
+//  Created by Josh Madison on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BINGAI_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BINGAI TESTUITests/BINGAI_TESTUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BINGAI_TESTUITestsLaunchTests.swift
+//  BINGAI TESTUITests
+//
+//  Created by Josh Madison on 1/11/24.
+//
+
+import XCTest
+
+final class BINGAI_TESTUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
